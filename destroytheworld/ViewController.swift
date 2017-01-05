f5482dcf51052055bb861bfffe3a40514784c2e2
Todd Andrews <tandrews@smithjs-macbook-pro.local>
2017-01-05 15:55:33 -0500

    one more change

--- destroytheworld/ViewController.swift
+++ destroytheworld/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Hello World, It's my world")
+        print("Hello World, It's my world...One more change!")
     }
 
     override func didReceiveMemoryWarning() {
